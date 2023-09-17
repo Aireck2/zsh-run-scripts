3df8021b8183c6f28a2e8690e8cac50f3f9268c8
@erick <erickescribaa@gmail.com>
2023-09-17 15:56:07 -0500

    chore: add run scripts logic (#1)

---
+++ zsh-run-scripts.zsh
@@ -0,0 +1,30 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+#
+# Authors:
+#   Erick Escriba <erickescribaa@gmail.com>
+#
+RUNNER_PLUGIN_DIR="$(dirname "${0}")"
+RUNNER_SOURCE_PATH="${RUNNER_PLUGIN_DIR}"/src
+
+# shellcheck source=/dev/null
+source "${RUNNER_SOURCE_PATH}"/base.zsh
+
+# shellcheck source=/dev/null
+source "${RUNNER_SOURCE_PATH}"/search.zsh
+
+function scripts::run {
+    local command
+    local runner
+    runner="$(search::runner)"
+    command="$(search::scripts)"
+
+    if [ -z "${command}" ]; then
+        return
+    fi
+
+    env "${runner} ${command}" && zle accept-line
+}
+
+zle -N scripts::run
+bindkey '^Xr' scripts::run
